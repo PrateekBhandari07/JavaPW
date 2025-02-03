054b3774a02ce7b5a06553a39cdcd8e2a1b70f57
PrateekBhandari07 <prateekbhandari1441@gmail.com>
2025-02-03 13:42:55 +0530

    Pattern 'N'

---
+++ pattern/Pattern06.java
@@ -0,0 +1,25 @@
+package JavaPW.pattern;
+import java.util.Scanner;
+public class Pattern06
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of rows and columns : ");
+        int row=sc.nextInt(),column=sc.nextInt();
+        for(int i=0;i<row;i++){
+            for(int j=0;j<column;j++){
+                if(j==0 || j==column-1 || i==j ){
+                    System.out.print("* ");
+                }
+                else System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
+//output
+
+//*     *
+//* *   *
+//*   * *
+//*     *
