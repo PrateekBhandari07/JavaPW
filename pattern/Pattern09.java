f805e7a9bc5cfa8ccf97f0e7704ec2557be0eaec
PrateekBhandari07 <prateekbhandari1441@gmail.com>
2025-02-06 07:58:57 +0530

    Pattern 'A'

---
+++ pattern/Pattern09.java
@@ -0,0 +1,27 @@
+package JavaPW.pattern;
+import java.util.Scanner;
+//  * * *
+//*       *
+//*       *
+//*       *
+//* * * * *
+//*       *
+//*       *
+//*       *
+public class Pattern09
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter length : ");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if((i==0 && j>0 && j<((n)/2))||  (i==n/2 && j<n/2)|| (j==0&&i>0)||(j==n/2&&i>0)){
+                        System.out.print("* ");
+                }
+                else System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
