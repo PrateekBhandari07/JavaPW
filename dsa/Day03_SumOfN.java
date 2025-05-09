4ed4208942fb30fe3fae1bb839e3c3f00ccbe5a5
PrateekBhandari07 <prateekbhandari1441@gmail.com>
2025-05-09 07:14:04 +0530

    "Sum of N numbers"

---
+++ dsa/Day03_SumOfN.java
@@ -0,0 +1,12 @@
+package JavaPW.dsa;
+
+public class Day03_SumOfN
+{
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println("Sum of N numbers = "+(n*(n+1)/2));
+        System.out.println("Handshake = "+(n*(n-1)/2));
+
+
+    }
+}
