66340fd97f03db7c8e7e78f7776b86752160f0f2
PrateekBhandari07 <prateekbhandari1441@gmail.com>
2025-05-11 20:21:46 +0530

    "Find second largest element in an Array"

---
+++ dsa/Day05_SecondLargest.java
@@ -0,0 +1,29 @@
+package JavaPW.dsa;
+public class Day05_SecondLargest
+{
+    public static void main(String[] args) {
+        int arr[]={5,5,2,5,2,1};
+        int firstLargest=Integer.MIN_VALUE;
+        int secondLargest=Integer.MIN_VALUE;
+        if(arr.length<2){
+            System.out.println("Array has only one element");
+            return;
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>firstLargest){
+                secondLargest=firstLargest;
+                firstLargest=arr[i];
+            }
+            else if(arr[i]>secondLargest && arr[i]!=firstLargest){
+                secondLargest=arr[i];
+            }
+        }
+        if(firstLargest==Integer.MIN_VALUE){
+            System.out.println("Array is filled with same value");
+        }
+        else{
+            System.out.println("First Largest : "+firstLargest);
+            System.out.println("Second Largest : "+secondLargest);
+        }
+    }
+}
