be701873e892527074d4a8a5e382c15299109418
PrateekBhandari07 <prateekbhandari1441@gmail.com>
2025-05-20 08:17:47 +0530

    "Updated Fibonacci Series Using Recursion"

--- dsa/Day06_FibonacciRecursion.java
+++ dsa/Day06_FibonacciRecursion.java
@@ -8,8 +8,7 @@ public class Day06_FibonacciRecursion {
         }
     }
    static int fibonacci(int n){
-      if(n<=0) return 0;
-      else if(n==1) return 1;
-     else  return fibonacci(n-1)+fibonacci(n-2);
+      if(n<=1) return n;
+      return fibonacci(n-1)+fibonacci(n-2);
     }
 }
