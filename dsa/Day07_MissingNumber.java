ae39b004779d7e9a41e6cdc868e7ca6564720106
PrateekBhandari07 <prateekbhandari1441@gmail.com>
2025-05-20 21:28:50 +0530

    "find the missing number in the array"

--- dsa/Day07_MissingNumber.java
+++ dsa/Day07_MissingNumber.java
@@ -3,6 +3,8 @@ package JavaPW.dsa;
 public class Day07_MissingNumber {
     public static void main(String[] args) {
 
+//        Brute Force Approach O(n^2)
+
 //        for(int i=1;i<=arr.length;i++){
 //            boolean flag=false;
 //            for(int j=0;j<arr.length;j++){
@@ -14,14 +16,17 @@ public class Day07_MissingNumber {
 //            }
 //            if(!flag) System.out.println(i);
 //        }
-        int arr[]={1,5,2,7,3,4};
+
+//        Better approach of time complexity O(2n)
+
+        int arr[]={1,1,1,5,2,2,7,3,4};
         int arr2[]=new int[(arr.length+2)];
         for(int i=1;i<arr2.length-1;i++){
-//            System.out.print(arr2[i]);
             arr2[arr[i-1]]=arr[i-1];
 //            System.out.println(arr[i-1]+" " +arr2[arr[i-1]]);
         }
         for(int i=1;i<arr2.length-1;i++){
+//            System.out.println(arr2[i-1]);
             if(arr2[i]==0){
                 System.out.println(i);
                 return;
